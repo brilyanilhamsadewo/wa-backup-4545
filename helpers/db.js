9862c14225322c53294e7fb582ce6a0fcc7b64e4
Brilyan Ilham Sadewo <50389985+brilyanilhamsadewo@users.noreply.github.com>
2022-03-16 17:00:44 +0700

    Add files via upload

--- helpers/db.js
+++ helpers/db.js
@@ -1,17 +1,28 @@
 const mysql = require('mysql2/promise');
+const { readSession } = require('./session');
 
+// const createConnection = async () => {
+//     return await mysql.createConnection({
+//         host:'remotemysql.com',
+//         user: 's4uZtE1YET',
+//         password: 'y1KjJ02SdI',
+//         database: 's4uZtE1YET'
+//     });
+// }
+
+// aws reblood rds
 const createConnection = async () => {
     return await mysql.createConnection({
-        host:'remotemysql.com',
-        user: 's4uZtE1YET',
-        password: 'y1KjJ02SdI',
-        database: 's4uZtE1YET'
+        host:'bot-wa-mysql.cepilawr0dy9.ap-southeast-1.rds.amazonaws.com',
+        user: 'bot_wa_mysql',
+        password: 'admindatabase123',
+        database: 'bot_wa_mysql'
     });
 }
 
 const getReply = async(keyword) => {
     const connection = await createConnection();
-    const [rows] = await connection.execute('SELECT message FROM wa_replies WHERE keyword=? COLLATE utf8_unicode_ci',[keyword]);
+    const [rows] = await connection.execute('SELECT message FROM wa_replies WHERE keyword=?',[keyword]);
     if(rows.length > 0) return rows[0].message;
     return false;
 }
