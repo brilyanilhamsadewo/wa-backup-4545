b1b19e69295aebbacea103822d8523476e5e7775
Brilyan Ilham Sadewo <50389985+brilyanilhamsadewo@users.noreply.github.com>
2022-03-16 15:02:58 +0700

    update pg

--- helpers/session.js
+++ helpers/session.js
@@ -2,8 +2,13 @@ const { query } = require('express-validator');
 const { Client } = require('pg');
 
 const client = new Client({
-  //connectionString: process.env.DATABASE_URL,
- connectionString: 'postgres://lkodvgskogdahg:70f4350351d7a8dd72f55a1867de3021a3a53ab723955045589ff271181ab34b@ec2-54-158-26-89.compute-1.amazonaws.com:5432/d9n1m32948am93',
+//   connectionString: process.env.DATABASE_URL,
+//   connectionString: 'postgres://pargpknghdabhf:91e0af5d28f2d19b86f9d3876b73ab71a472030a2ec74be82a4331701e88573e@ec2-34-238-37-113.compute-1.amazonaws.com:5432/dcb0k81v2qt9b6',
+  user: 'databaselogin',
+  host: 'database-wa.cepilawr0dy9.ap-southeast-1.rds.amazonaws.com',
+  database: 'wa_bot',
+  password: 'admindatabase123',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
   }
