8a0d3ca91e115ef3f91a3605d95b0a11c41c0916
Brilyan Ilham Sadewo <50389985+brilyanilhamsadewo@users.noreply.github.com>
2022-05-07 06:18:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -232,8 +232,13 @@ app.use(fileUpload({
   });
   
   
-  const checkRegisteredNumber = async function(number) {
-    const isRegistered = await client.isRegisteredUser(number);
+//   const checkRegisteredNumber = async function(number) {
+//     const isRegistered = await client.isRegisteredUser(number);
+//     return isRegistered;
+//   }
+  
+  const checkRegisteredNumber = function(number) {
+    const isRegistered = client.isRegisteredUser(number);
     return isRegistered;
   }
   
@@ -258,7 +263,8 @@ app.use(fileUpload({
     const number = phoneNumberFormatter(req.body.number);
     const message = req.body.message;
   
-    const isRegisteredNumber = await checkRegisteredNumber(number);
+//     const isRegisteredNumber = await checkRegisteredNumber(number);
+    const isRegisteredNumber = checkRegisteredNumber(number);
   
     if (!isRegisteredNumber) {
       return res.status(422).json({
@@ -377,47 +383,47 @@ app.use(fileUpload({
   // });
   
   // Clearing message on spesific chat
-  app.post('/clear-message', [
-    body('number').notEmpty(),
-  ], async (req, res) => {
-    const errors = validationResult(req).formatWith(({
-      msg
-    }) => {
-      return msg;
-    });
+//   app.post('/clear-message', [
+//     body('number').notEmpty(),
+//   ], async (req, res) => {
+//     const errors = validationResult(req).formatWith(({
+//       msg
+//     }) => {
+//       return msg;
+//     });
   
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        status: false,
-        message: errors.mapped()
-      });
-    }
+//     if (!errors.isEmpty()) {
+//       return res.status(422).json({
+//         status: false,
+//         message: errors.mapped()
+//       });
+//     }
   
-    const number = phoneNumberFormatter(req.body.number);
+//     const number = phoneNumberFormatter(req.body.number);
   
-    const isRegisteredNumber = await checkRegisteredNumber(number);
+//     const isRegisteredNumber = await checkRegisteredNumber(number);
   
-    if (!isRegisteredNumber) {
-      return res.status(422).json({
-        status: false,
-        message: 'The number is not registered'
-      });
-    }
+//     if (!isRegisteredNumber) {
+//       return res.status(422).json({
+//         status: false,
+//         message: 'The number is not registered'
+//       });
+//     }
   
-    const chat = await client.getChatById(number);
+//     const chat = await client.getChatById(number);
     
-    chat.clearMessages().then(status => {
-      res.status(200).json({
-        status: true,
-        response: status
-      });
-    }).catch(err => {
-      res.status(500).json({
-        status: false,
-        response: err
-      });
-    })
-  });
+//     chat.clearMessages().then(status => {
+//       res.status(200).json({
+//         status: true,
+//         response: status
+//       });
+//     }).catch(err => {
+//       res.status(500).json({
+//         status: false,
+//         response: err
+//       });
+//     })
+//   });
   
   server.listen(port, function() {
     console.log('App running on *: ' + port);
