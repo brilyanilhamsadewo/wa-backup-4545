507dad50ff29cd94a282a298265c661447f2a198
Brilyan Ilham Sadewo <50389985+brilyanilhamsadewo@users.noreply.github.com>
2022-05-07 06:37:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -53,9 +53,27 @@ app.use(fileUpload({
 //     })
 //   });
 
-  const client = new Client({
-    authStrategy: new LocalAuth()
-  });
+//   const client = new Client({
+//     authStrategy: new LocalAuth()
+//   });
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
+});
 
 
 //   const savedSession = await sesi.readSession();
